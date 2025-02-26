bc4d07283ac472d808b3a38903ef769ff0b40dc2
whatisaProCoder <debnathpritam2005@gmail.com>
2025-02-26 18:38:43 +0530

    servo

--- ServoBasicControl/ServoBasicControl.ino
+++ ServoBasicControl/ServoBasicControl.ino
@@ -2,19 +2,16 @@
 
 Servo myServo;
 
+int angle_difference = 20;
+
 void setup() {
+  Serial.begin(115200);
   myServo.attach(13);  // Connect servo signal to GPIO 13
 }
 
 void loop() {
-  for(int i=60;i<=120;i++) {
-    myServo.write(i);
-    delay(10);
-  }
+  myServo.write(90+angle_difference);
   delay(1000);
-  for(int i=120;i>=60;i--) {
-    myServo.write(i);
-    delay(10);
-  }
+  myServo.write(90-angle_difference);
   delay(1000);
 }
