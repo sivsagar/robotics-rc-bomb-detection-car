fb5121df7cd6ebfe14ade284ff2294e02cfa9aef
sivsagar <sivsagar29112006@gmail.com>
2025-02-26 08:54:40 +0530

    fix2

--- GPSNEOM8N./gpsneo_m8n.ino
+++ GPSNEOM8N./gpsneo_m8n.ino
@@ -1,17 +1,12 @@
 #include <TinyGPS++.h>
 #include <HardwareSerial.h>
 
-// Define the hardware serial port for GPS
-HardwareSerial gpsSerial(1);
-
-// Create a TinyGPS++ object
 TinyGPSPlus gps;
+HardwareSerial gpsSerial(1);  // Use UART1
 
 void setup() {
-    Serial.begin(115200);    // Serial monitor
-    gpsSerial.begin(9600, SERIAL_8N1, 16, 17);  // GPS module (RX=16, TX=17)
-    
-    Serial.println("GPS Module Initialized...");
+    Serial.begin(115200);
+    gpsSerial.begin(9600, SERIAL_8N1, 16, 17);  // GPS RX=16, TX=17
 }
 
 void loop() {
@@ -19,23 +14,12 @@ void loop() {
         gps.encode(gpsSerial.read());
 
         if (gps.location.isUpdated()) {
-            Serial.print("Latitude: ");
-            Serial.print(gps.location.lat(), 6);
-            Serial.print(", Longitude: ");
-            Serial.println(gps.location.lng(), 6);
-        }
-
-        if (gps.speed.isUpdated()) {
-            Serial.print("Speed (km/h): ");
-            Serial.println(gps.speed.kmph());
-        }
-
-        if (gps.satellites.isUpdated()) {
-            Serial.print("Satellites: ");
-            Serial.println(gps.satellites.value());
+            Serial.print("Lat: "); Serial.print(gps.location.lat(), 6);
+            Serial.print(", Lng: "); Serial.println(gps.location.lng(), 6);
         }
     }
 }
+
 
 /*Explanation
 ->Uses HardwareSerial(1) since ESP32 has multiple hardware UARTs.
