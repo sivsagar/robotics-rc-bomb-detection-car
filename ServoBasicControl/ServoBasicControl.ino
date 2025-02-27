7b6b928fa1b3b3e99f153cd34174d2e54c7c711a
whatisaProCoder <debnathpritam2005@gmail.com>
2025-02-27 11:22:41 +0530

    fix

--- ServoBasicControl/ServoBasicControl.ino
+++ ServoBasicControl/ServoBasicControl.ino
@@ -2,16 +2,24 @@
 
 Servo myServo;
 
+// Steering Angle
+#define LEFT_ANGLE 60
+#define MIDDLE_ANGLE 90
+#define RIGHT_ANGLE 140
+
 void setup() {
   Serial.begin(115200);
   myServo.attach(13);  // Connect servo signal to GPIO 13
 }
 
 void loop() {
-  myServo.write(80);
-  Serial.println(80);
+  myServo.write(LEFT_ANGLE);
+  Serial.println(LEFT_ANGLE);
   delay(1000);
-  myServo.write(150);
-  Serial.println(150);
+  myServo.write(MIDDLE_ANGLE);
+  Serial.println(MIDDLE_ANGLE);
+  delay(1000);
+  myServo.write(RIGHT_ANGLE);
+  Serial.println(RIGHT_ANGLE);
   delay(1000);
 }
